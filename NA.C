3eb31248695302e86253d9ebf394b9577a131666
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-01-30 16:34:02 +0530

    Create NA.C

---
+++ NA.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char N;
+printf("enter the letter:"N);
+scanf("%d",ch);
+{
+if((N>65 && N <=90)||(N>=92 && N <=122))
+printf("%c is a not alphabet",N);
+return 0;
+}
+
