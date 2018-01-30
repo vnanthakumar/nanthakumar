67f83543bef3124804446f8d59f4cfa18e654c02
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-01-30 16:02:55 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double number;
+
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+
+    if (number <= 10000)
+    {
+        if (number == 10000)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative number.");
+    }
+    else
+        printf("You entered a positive number.");
+    return 0;
+}
