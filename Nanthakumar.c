21f56cec13bdb43c0232698ba4d91269285a219e
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-01-30 16:16:20 +0530

    Create Nanthakumar.c

---
+++ Nanthakumar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+    int n;
+    printf("Enter the a value ");
+    scanf("%d",&n);
+if(a>0)
+{
+    printf("Positive");
+}
+    else if(n<0)
+    {
+        printf("Negative");
+    }
+
+    else
+    {
+        printf("Zero");
+    }
+getch();
+}
