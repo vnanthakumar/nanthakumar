ec1918a1faca7db7199a79874632a329d2d19cbc
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-01-30 16:29:21 +0530

    Rename nan.c to nant.c

---
+++ nant.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char m;
+clrscr();
+printf("enter a character");
+scanf("%c",&n);
+if ((m==a||m==e||m==i||m==o||m==u)
+{
+printf("the character is vowel");
+}
+else
+{
+printf("the character is cosonent");
+}
+getch();
+}
+© 2018 GitHub, Inc.
