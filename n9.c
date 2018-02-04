d42a0964853e37f0c83ad8f6c212710ea0026b7a
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-02-04 15:38:09 +0530

    Create n9.c

---
+++ n9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int base, exponent;
+long long result = 1;
+  {
+printf("Enter a base number: ");
+  }
+scanf("%d", &base);
+  {
+printf("Enter an exponent: ");
+  }
+scanf("%d", &exponent);
+while (exponent != 0)
+{
+result *= base;
+--exponent;
+}
+printf("Answer = %lld", result);
+
+}
