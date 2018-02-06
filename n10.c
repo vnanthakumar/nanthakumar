e25513f80fa3d5e276760f7908a0a32ab8ef59dd
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-02-06 16:13:20 +0530

    Create n10.c

---
+++ n10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,i,d=0;
+clrscr();
+scanf("%d",&a);
+b=a/2;
+for(i=2;i<=b;i++)
+{
+if(a%i==0)
+{
+d=1;
+break;
+}
+}
+if(d==0)
+printf("YES");
+else
+printf("NO");
+getch();
+}
