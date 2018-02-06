418969af1c733e4e269b9233a5e50803928f7d60
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-02-06 16:21:48 +0530

    Create n11.c

---
+++ n11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter the a,b value");
+scanf("%d %d&a&b");
+if(a>b)
+{
+printf("\n a is even number ");
+else
+printf(" a is odd number ");
+printf("odd numbers between two intervals");
+getch();
+
+}
