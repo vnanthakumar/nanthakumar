080f363c086ea5ffea3a6f36c770b4a5750b75f6
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-02-13 15:51:40 +0530

    Create n19.c

---
+++ n19.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=1,i,n;
+    printf("Enter the values\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        a=a*i;
+        
+        
+    }
+    printf("%d",a);
+    
+}
