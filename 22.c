4706854ce320a1f8b622cd1fcac16099dbff1de9
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-03-01 16:48:51 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a[n],i,n,max;
+printf("enter the number of elements");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+scanf("%d",&a[n]);
+max=array[0];
+{
+for(i=0;i<n;i++)
+if(max<a[n])
+{
+max=a[n];
+i++;
+}
+printf("%d",&max);
+}
+}
