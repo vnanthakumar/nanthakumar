252927f39acb319ac6dc178bd0ec8708b26db01d
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-03-03 16:42:49 +0530

    Update 40.c

--- 40.c
+++ 40.c
@@ -2,14 +2,14 @@
 void main()
 {
 int f1=0,f2=1,n,i,c;
-printf("enter a number");
-scanf("%d",&n);
-printf("%d%d",f1,f2);
+printf("enter a number\n");
+scanf("%d\n",&n);
+printf("%d%d\n",f1,f2);
 for(i=1;i<n;i++)
 {
 c=f1+f2;
 f1=f2;
 f2=c;
-printf("%d",c);
+printf("%d\n",c);
 }
 }
