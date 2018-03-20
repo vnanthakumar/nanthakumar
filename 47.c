013b2f80eed8481823649ea2fbc67a253f78a916
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-03-20 04:22:54 -0700

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#includ<conio.h>
+void main()
+{
+    int a=10;
+    int b=20;
+    int c=30;
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    if(b<c)
+    {
+        printf("%d",c);
+    }
+
+}
