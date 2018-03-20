6492ea6c991e47d378020518d6a88a3aa5f88776
vnanthakumar <35652654+vnanthakumar@users.noreply.github.com>
2018-03-20 04:14:44 -0700

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[100];
+	scanf("%s",a);
+	int n,i,flag;
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>=0&&a[i]<=9)
+		flag=1;
+		else
+		{
+			flag=0;
+			printf("No");
+			break;
+		}
+	}
+	if(flag==1)
+	printf("Yes");
+}
